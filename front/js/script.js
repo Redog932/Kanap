0bc55920a8aab8e51bb14db9a5602d1012391d38
Redouane <Redouane.pro@outlook.com>
2023-01-11 01:10:37 +0100

    Test recuperation de données

---
+++ front/js/script.js
@@ -0,0 +1,29 @@
+// Recuperation info du tableau
+
+const url = "http://localhost:3000/api/products"
+
+fetch(url)  .then((response) => response.json())  .then((json) => console.log(json));
+
+ 
+// **Recuperation des items
+
+const items = document.getElementById(items)
+
+const link = document.createElement("a")
+this.href = "http://localhost:3000/images/kanap02.jpeg"
+this.text = "Photo d'un canapé bleu, deux places"
+
+
+
+// Test d'objet
+
+// canape (colors, description, image, name, price, id) {
+//     colors = colors,
+//     description =description,
+//     image = image,
+//     name = name, 
+//     price = price,
+//     id = id
+// }
+ 
+ 
