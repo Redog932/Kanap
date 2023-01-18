8fa20f885a2c7277841a7d81df441981f151769f
Redouane <Redouane.pro@outlook.com>
2023-01-18 23:37:03 +0100

    Boucle pour fiches produit

--- front/js/script.js
+++ front/js/script.js
@@ -2,28 +2,54 @@
 
 const url = "http://localhost:3000/api/products"
 
-fetch(url)  .then((response) => response.json())  .then((json) => console.log(json));
-
- 
-// **Recuperation des items
-
-const items = document.getElementById(items)
-
-const link = document.createElement("a")
-this.href = "http://localhost:3000/images/kanap02.jpeg"
-this.text = "Photo d'un canapé bleu, deux places"
+fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
 
 
 
-// Test d'objet
+        const pieces = data;
+        const articles = pieces[0];
+        console.log(pieces)
 
-// canape (colors, description, image, name, price, id) {
-//     colors = colors,
-//     description =description,
-//     image = image,
-//     name = name, 
-//     price = price,
-//     id = id
-// }
- 
- 
+        // **test Boucle For
+        for (let i = 0; i < pieces.length; i++) {
+            console.log("coucou")
+
+            
+            // Récupération de l'élément du DOM qui accueillera les fiches
+            const itemsElement = document.createElement("article");
+
+            const sectionItems = document.getElementById("items");
+            const link = document.createElement("a");
+
+            const colorsElement = document.createElement("p");
+            colorsElement.innerText = pieces[i].colors;
+
+            const altTxtElement = document.createElement("alt");
+            altTxtElement.innerText = pieces[i].altTxt;
+
+            const descriptionElement = document.createElement("p");
+            descriptionElement.innerText = pieces[i].description;
+
+            const imageElement = document.createElement("img");
+            imageElement.src = pieces[i].imageUrl;
+
+            const nameElement = document.createElement("h2");
+            nameElement.innerText = pieces[i].name;
+
+            const priceElement = document.createElement("p");
+            priceElement.innerText = `Prix: ${pieces[i].price} €`;
+
+
+            link.appendChild(itemsElement);
+            itemsElement.appendChild(imageElement);
+            itemsElement.appendChild(nameElement);
+            imageElement.appendChild(altTxtElement)
+            itemsElement.appendChild(descriptionElement);
+            itemsElement.appendChild(priceElement);
+            sectionItems.appendChild(link);
+            
+        }
+    })
